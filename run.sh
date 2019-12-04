41ee0e8eba22b0898dd7846dbc5001281d4f9dc1
Jia <jia@talentful.ai>
2019-12-04 15:58:50 -0800

    remove pycache

--- run.sh
+++ run.sh
@@ -5,28 +5,28 @@ PATH=${CUDA_HOME}/bin:${PATH}
 
 export PATH
 
-mkdir t0w2v
-python3 conv_net_train_gpu.py -static -word2vec 0
-mv cvt* t0w2v
-mv perf_output_* t0w2v
+mkdir t0w2v_keras
+python3 conv_net_train_keras.py -static -word2vec 0
+mv cvt* t0w2v_keras
+mv perf_output_* t0w2v_keras
 
-mkdir t1w2v
-python3 conv_net_train_gpu.py -static -word2vec 1
-mv cvt* t1w2v
-mv perf_output_* t1w2v
+mkdir t1w2v_keras
+python3 conv_net_train_keras.py -static -word2vec 1
+mv cvt* t1w2v_keras
+mv perf_output_* t1w2v_keras
 
-mkdir t2w2v
-python3 conv_net_train_gpu.py -static -word2vec 2
-mv cvt* t2w2v
-mv perf_output_* t2w2v
+mkdir t2w2v_keras
+python3 conv_net_train_keras.py -static -word2vec 2
+mv cvt* t2w2v_keras
+mv perf_output_* t2w2v_keras
 
-mkdir t3w2v
-python3 conv_net_train_gpu.py -static -word2vec 3
-mv cvt* t3w2v
-mv perf_output_* t3w2v
+mkdir t3w2v_keras
+python3 conv_net_train_keras.py -static -word2vec 3
+mv cvt* t3w2v_keras
+mv perf_output_* t3w2v_keras
 
 
-mkdir t4w2v
+mkdir t4w2v_keras
 python3 conv_net_train_gpu.py -static -word2vec 4
-mv cvt* t4w2v
-mv perf_output_* t4w2v
+mv cvt* t4w2v_keras
+mv perf_output_* t4w2v_keras
